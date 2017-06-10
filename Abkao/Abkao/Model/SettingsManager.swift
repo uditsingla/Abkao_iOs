a4f637ad52e1200f66e445f6a4afa3234444e41f
abhisheksingla <abhishek.singhla@sourcefuse.com>
2017-06-11 01:45:18 +0530

    Modal

---
+++ Abkao/Abkao/Model/SettingsManager.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsManager.swift
+//  Abkao
+//
+//  Created by Abhishek Singla on 10/06/17.
+//  Copyright © 2017 Abkao. All rights reserved.
+//
+
+import UIKit
+
+class SettingsManager: NSObject {
+
+}
